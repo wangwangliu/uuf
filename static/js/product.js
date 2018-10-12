dae144c765601b2571e68d43501ca194a52b1771
Storys.Zhang <storys.zhang@gmail.com>
2018-10-12 19:31:34 +0800

    add product

--- static/js/product.js
+++ static/js/product.js
@@ -1,6 +1,10 @@
+var rootUrl = "http://120.55.41.76";
+
 $('.product-nav-title').click(function (event) {
   var event = event || e;
   $(".product-title").text($(this).text());
+  var fileName = $(this).attr("file-name");
+  loadProduct(fileName);
 });
 
 $('.product-nav-content').click(function (event) {
@@ -8,6 +12,44 @@ $('.product-nav-content').click(function (event) {
   $(".product-title").text($(this).text());
 });
 
-$('.product-item').click(function () {
-  document.location.href = "detail.html"
-})
+
+function bindDetail(){
+  $('.product-item').click(function () {
+    document.location.href = "detail.html"
+  });
+}
+
+
+$(function(){
+  var pathname = window.location.pathname;
+  var id = pathname.substr(pathname.length-1, pathname.length);
+  $('.product-nav-title').each(function(i,n){
+    if(id === $(n).attr("data")){
+      $(".product-title").text($(n).text());
+      loadProduct($(n).attr("file-name"))
+    }
+  })
+});
+
+function loadProduct(fileName){
+    $.ajax({
+      type:"GET",
+      url:rootUrl+"/ufc-data/data/"+fileName,
+      dataType: "json",
+      success: function(data){
+        if(data.length == 0){
+            $(".product-box").html("暂无符合的产品");
+            return;
+           }
+          var str='';
+          $.each(data,function(i,n){
+              str+='<div class="product-item" data=>';
+              str+='<img class="product-item-img" src="'+ (rootUrl + n.mainImg) +'" />';
+              str+='<div class="product-item-intro">'+ n.title +'</div>';
+              str+="</div>";
+          });
+          $(".product-box").html(str);
+          bindDetail();
+      }
+  });
+}
