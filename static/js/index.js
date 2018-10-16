2bf0d1fc3106567ae0479404b414c2773527af66
storys.zhang <storys.zhang@gmail.com>
2018-10-17 02:06:03 +0800

    广告

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,30 @@
 function goLinkk(URL) {
     location.href = URL;
 }
+
+var rootUrl = "http://120.55.41.76";
+
+$(function(){
+	loadAdvertising();
+})
+
+function loadAdvertising(){
+	$.ajax({
+        type: "GET",
+        url: rootUrl + "/ufc-data/data/UFC-Advertising.json",
+        dataType: "json",
+        success: function(data) {
+            var str = '';
+            $.each(data, function(i, n) {
+                if (i == 0) {
+                	$(".content-top").click(function(){
+                		window.open(n.url);
+                	});
+                }
+            });
+
+            $(".content-top").html(str);
+
+        }
+    });
+}
