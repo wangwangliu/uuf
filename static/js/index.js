a5640d6887e00a74ce4f7e162d800429011f9a2d
liuriwang <tryhot@163.com>
2018-10-17 01:00:33 +0800

    feat

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,3 @@
-$(function() {
-    var galleryTop = new Swiper('.index-gallery-top', {
-        slidesPerView: 3,
-        spaceBetween: 20,
-        loop: true,
-        loopedSlides: 5, //looped slides should be the same
-    });
-
-})
-
 function goLinkk(URL) {
     location.href = URL;
 }
